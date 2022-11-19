2ec130596cd8c13d045d76105b87c6801917b003
Prem mishra <118355053+Prem9897@users.noreply.github.com>
2022-11-20 00:47:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001561993353"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("Elisa")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Elisa")
 
 # Your User ID.
 OWNER_ID = list(
